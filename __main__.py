ce69e24b613224fadc356e58c17af3a1f4491dc2
redboxlogo <redboxlogo@protonmail.com>
2024-01-30 14:52:40 -0800

    __main__.py comments for test code
    per line comments in __main__.py

--- __main__.py
+++ __main__.py
@@ -4,21 +4,25 @@ from time import process_time
 from random import choice, randrange
 
 # Student Id and 4 grades
-db = Database()
-grades_table = db.create_table('Grades', 5, 0)
-query = Query(grades_table)
-keys = []
+db = Database()                                                             # create database object from db.py
+grades_table = db.create_table('Grades', 5, 0)                              # call create_table() to create table from db.py
+query = Query(grades_table)                                                 # call Query() to create Query object rom query.py 
+keys = []                                                                   # create local "keys" variable
 
-insert_time_0 = process_time()
-for i in range(0, 10000):
-    query.insert(906659671 + i, 93, 0, 0, 0)
-    keys.append(906659671 + i)
-insert_time_1 = process_time()
+################################################################################################################################
 
-print("Inserting 10k records took:  \t\t\t", insert_time_1 - insert_time_0)
+insert_time_0 = process_time()                                              #get time
+for i in range(0, 10000):                                                   #for loop for query inserts and key appends
+    query.insert(906659671 + i, 93, 0, 0, 0)                                #call insert() from query
+    keys.append(906659671 + i)                                              #call append() from key
+insert_time_1 = process_time()                                              #get time
+
+print("Inserting 10k records took:  \t\t\t", insert_time_1 - insert_time_0) #print
+
+################################################################################################################################
 
 # Measuring update Performance
-update_cols = [
+update_cols = [                                                             #create "update_cols" matrix
     [None, None, None, None, None],
     [None, randrange(0, 100), None, None, None],
     [None, None, randrange(0, 100), None, None],
@@ -26,31 +30,37 @@ update_cols = [
     [None, None, None, None, randrange(0, 100)],
 ]
 
-update_time_0 = process_time()
-for i in range(0, 10000):
-    query.update(choice(keys), *(choice(update_cols)))
-update_time_1 = process_time()
-print("Updating 10k records took:  \t\t\t", update_time_1 - update_time_0)
+update_time_0 = process_time()                                              #get time 
+for i in range(0, 10000):                                                   #update 10k queries
+    query.update(choice(keys), *(choice(update_cols)))                      #call update() from query
+update_time_1 = process_time()                                              #get time
+print("Updating 10k records took:  \t\t\t", update_time_1 - update_time_0)  #print
+
+################################################################################################################################
 
 # Measuring Select Performance
-select_time_0 = process_time()
-for i in range(0, 10000):
-    query.select(choice(keys),0 , [1, 1, 1, 1, 1])
-select_time_1 = process_time()
-print("Selecting 10k records took:  \t\t\t", select_time_1 - select_time_0)
+select_time_0 = process_time()                                              #get time
+for i in range(0, 10000):                                                   #select 10k records
+    query.select(choice(keys),0 , [1, 1, 1, 1, 1])                          #call select() from query
+select_time_1 = process_time()                                              #get time
+print("Selecting 10k records took:  \t\t\t", select_time_1 - select_time_0) #print
+
+################################################################################################################################
 
 # Measuring Aggregate Performance
-agg_time_0 = process_time()
-for i in range(0, 10000, 100):
-    start_value = 906659671 + i
-    end_value = start_value + 100
-    result = query.sum(start_value, end_value - 1, randrange(0, 5))
-agg_time_1 = process_time()
-print("Aggregate 10k of 100 record batch took:\t", agg_time_1 - agg_time_0)
+agg_time_0 = process_time()                                                 #get time
+for i in range(0, 10000, 100):                                              #for loop
+    start_value = 906659671 + i                                             #create start value
+    end_value = start_value + 100                                           #create end value
+    result = query.sum(start_value, end_value - 1, randrange(0, 5))         #call sum() from query
+agg_time_1 = process_time()                                                 #get time
+print("Aggregate 10k of 100 record batch took:\t", agg_time_1 - agg_time_0) #print
+
+################################################################################################################################
 
 # Measuring Delete Performance
-delete_time_0 = process_time()
-for i in range(0, 10000):
-    query.delete(906659671 + i)
-delete_time_1 = process_time()
-print("Deleting 10k records took:  \t\t\t", delete_time_1 - delete_time_0)
+delete_time_0 = process_time()                                              #get time
+for i in range(0, 10000):                                                   #for loop for delete
+    query.delete(906659671 + i)                                             #call delete from query
+delete_time_1 = process_time()                                              #get time
+print("Deleting 10k records took:  \t\t\t", delete_time_1 - delete_time_0)  #print
