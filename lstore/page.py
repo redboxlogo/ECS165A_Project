8e873f109fce22a69f8b451a93225439c08a39ac
redboxlogo <redboxlogo@protonmail.com>
2024-02-06 19:16:31 -0800

    testing bytearray()

--- lstore/page.py
+++ lstore/page.py
@@ -13,10 +13,24 @@ class Page:
 
     def write(self, RecordObj):
         self.num_records += 1
-        self.record_list.append(RecordObj)
+        # self.record_list.append(RecordObj)
         # print(bytearray(RecordObj.columns))
 
         # self.data = self.data.append(RecordObj.columns)
         #print(self.data)
+
+        for i in range(len(RecordObj.columns)):
+            self.data[i] = 5#RecordObj.columns[i]
+
         pass
 
+        # # Create a bytearray with 4096 elements initialized to 0
+        # my_bytearray = bytearray(4096)
+
+        # # Change the values of elements in the bytearray
+        # # For example, let's change the first 10 elements to a different value (e.g., 255)
+        # for i in range(20):
+        #     my_bytearray[i] = 8
+
+        # # Print the updated bytearray
+        # print(my_bytearray)
