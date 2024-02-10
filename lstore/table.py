3912c4f6008dd56bd1d9ab48ae11157b8331f431
Wanzhu Zheng <140910696+wanzhuz@users.noreply.github.com>
2024-02-10 01:33:32 -0800

    Update table.py with select() helper functions

--- lstore/table.py
+++ lstore/table.py
@@ -1,11 +1,20 @@
 from lstore.index import Index
 from lstore.page import Page
 from time import time
+import math
 
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
 TIMESTAMP_COLUMN = 2
 SCHEMA_ENCODING_COLUMN = 3
+
+# idk if we should put this in page?
+PAGE_SIZE = 4096
+BYTES_PER_ELEMENT = 8
+PAGES_PER_PRANGE = 16
+
+ELEMENTS_PER_PAGE = PAGE_SIZE / BYTES_PER_ELEMENT
+RECORDS_PER_PRANGE = PAGES_PER_PRANGE * ELEMENTS_PER_PAGE
 
 
 class Record:
@@ -65,3 +74,52 @@ class Table:
     def newBasePage(self, pageID):
         newBase = Page(pageID)                                          # create a base page
         return newBase                                                  # return newly created base page
+
+    def get_record_cols(self, search_key, baseRID):
+        base_index = self.get_page_index(baseRID)  # get base page index
+        base_offset = self.get_page_offset(baseRID)  # get base page offset
+        base_record = self.base_page[base_index].get_record(base_offset)  # fetch the record
+        base_pointer = base_record[INDIRECTION_COLUMN]  # get base indirection RID
+        schema = base_record[SCHEMA_ENCODING_COLUMN]  # retrieves schema encoding info stored in the col
+
+        if schema == 1:  # retrieves from tail page if schema = 1
+            tail_index = self.get_page_index(base_pointer)  # get tail page index
+            tail_offset = self.get_page_offset(base_pointer)  # get tail page offset
+            tail_record = self.tail_page[tail_index].get_record(tail_offset)  # fetch the record
+            record = Record(tail_record[RID_COLUMN], schema, search_key, tail_record[4:])
+        else:  # retrieves from base page if schema != 1
+            record = Record(base_record[RID_COLUMN], schema, search_key, base_record[4:])
+        return record
+
+    # calculate where RID is located in base pages
+    def get_page_index(self, RID):
+        return math.floor((RID % (PAGES_PER_PRANGE * ELEMENTS_PER_PAGE)) / ELEMENTS_PER_PAGE)
+
+    # count bytes until data starts
+    def get_page_offset(self, RID):
+        offset = RID
+        while offset >= RECORDS_PER_PRANGE:
+            offset -= RECORDS_PER_PRANGE
+        while offset >= ELEMENTS_PER_PAGE:
+            offset -= ELEMENTS_PER_PAGE
+        return offset
+
+    def get_record(self, offset):  # still need to work on this function, below is the pseudocode
+        record = []
+        # loop through each metadata column and read data at the given offset:
+            # read the data in the metadata column (where to access in metadata?)
+            # append data to record
+        # loop through each data column and read data at the given offset:
+            # read the data in the data column
+            # append data to record
+        # return record
+
+    def select(self, search_key, search_key_index, projected_query_columns):
+        if search_key not in self.key_map_RID:
+            print("RID does not exist for this key")
+            return False
+        baseRID = self.key_map_RID[search_key]
+        current_prange = math.floor(baseRID / RECORDS_PER_PRANGE)  # identify page range we are looking at
+        record = self.page_directory[current_prange].select(search_key, baseRID)
+        return [record]
+        
