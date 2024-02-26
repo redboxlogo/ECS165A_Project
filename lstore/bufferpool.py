4c4e50bc9e0cb76f65f1635a4ef928c0744d23bf
Wanzhu Zheng <140910696+wanzhuz@users.noreply.github.com>
2024-02-26 14:56:41 -0800

    updated full(), added frame_to_dict(), updated Frame class

--- lstore/bufferpool.py
+++ lstore/bufferpool.py
@@ -12,11 +12,26 @@ class Bufferpool():
         self.frame_count = 0  # frame count
 
     '''
+    passes frames to frame directory
+    '''
+    def frame_to_dict(self, table_name, bpage, brecord, frame_index):  # maybe add in page range?
+        frame_key = (table_name, bpage, brecord)
+        self.frame_directory[frame_key] = frame_index 
+        self.frames[frame_index].tuple_key = frame_key
+
+        if self.frame_count < BUFFERPOOL_FRAME_COUNT:
+            self.frame_count += 1
+            # print("Frame added successfully to bufferpool")
+
+    '''
     this function checks if our bufferpool is full
     returns True if full, returns False if not
     '''
     def full(self):
-        pass
+        if self.frame_count == BUFFERPOOL_FRAME_COUNT:
+            return True
+        else:
+            return False
 
     def fetch_page(self, page_id):
         # Implementation for fetching a page into the bufferpool
@@ -32,26 +47,40 @@ class Bufferpool():
 
 class Frame():
 
-    def __init__(self):
+    def __init__(self, table_name, disk_path2page):
         self.cols = []  # all column data
-        self.dirty = False  # indicates dirty bit
+        self.dirty = False  # indicates dirty page
         self.pinned = False  # indicates whether a frame is pinned or unpinned
         self.time = 0  # records time in bufferpool
+        self.access_count = 0  # number of times page has been accessed
+        self.tuple_key = None  # holds a tuple key that uniquely identifies the frame; temporary
+        self.disk_path2page = disk_path2page  # path to page on disk
+        self.table_name = table_name  # name of table associated with frame
 
     '''
-    setter function for dirty bits
+    setter function for dirty page
     '''
     def set_dirty(self):
         self.dirty = True
+        return True
+
+    '''
+    unset function for dirty page
+    '''
+    def unset_dirty(self):
+        self.dirty = False
+        return True
     
     '''
     indicates the frame is pinned
     '''
     def pin(self):
         self.pinned = True
+        return True
         
     '''
     indicates the frame has been unpinned
     '''
     def unpin(self):
         self.pinned = False
+        return True
