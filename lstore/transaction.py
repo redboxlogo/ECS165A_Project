1afc652a35f6ed0b4073b5454cc1e0f670d3a302
apunj4ni <158226605+apunj4ni@users.noreply.github.com>
2024-03-12 15:10:16 -0700

    commit and abort added to transaction

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -1,5 +1,8 @@
 from lstore.table import Table, Record
 from lstore.index import Index
+from lstore.locks import ReadWriteLock
+from lstore.query import Query
+import datetime
 
 class Transaction:
 
@@ -8,6 +11,16 @@ class Transaction:
     """
     def __init__(self):
         self.queries = []
+        self.name = None  # name of query function
+        self.fun = None  # query function
+        self.timestamp = datetime.now()  # timestamp
+        self.key = None  # key
+        self.column = None  # column
+        self.columns = None
+        self.start_loc = None
+        self.end_loc = None
+        self.locks = {}
+        self.read_write_lock = ReadWriteLock
         pass
 
     """
@@ -17,9 +30,17 @@ class Transaction:
     # t = Transaction()
     # t.add_query(q.update, grades_table, 0, *[None, 1, None, 2, None])
     """
+
     def add_query(self, query, table, *args):
+        if self.table is None:
+           # query_member = getmembers(query, lambda member: isinstance(member, Query))[0][1]
+            '''
+            getmembers() returns the member functions present in the module passed as an argument of this method
+            '''
+            self.table = query_member.table  # dumps the table data for the query object in table
         self.queries.append((query, args))
         # use grades_table for aborting
+        return
 
         
     # If you choose to implement this differently this method must still return True if transaction commits or False on abort
@@ -33,11 +54,29 @@ class Transaction:
 
     
     def abort(self):
-        #TODO: do roll-back and any other necessary operations
+        # Roll back changes by restoring the original state of all affected records
+        for record_id, original_record in self.original_states.items():
+            # Assuming Table class has a method to update records by ID
+            Table.update_record_by_id(record_id, original_record)
+        print("Transaction aborted and changes rolled back.")
         return False
 
     
     def commit(self):
-        # TODO: commit to database
-        return True
+        # Commit changes made during the transaction
+        try:
+            for rid, lock_type in self.locks.items():
+                if lock_type == 'read':
+                    # Release reader lock for the given record identifier
+                    self.read_write_lock.release_shared_lock(rid)
+                elif lock_type == 'write':
+                    # Release writer lock for the given record identifier
+                    self.read_write_lock.release_exclusive_lock(rid)
+            print("Transaction committed successfully.")
+            return True
+        except Exception as e:
+            # Handle exceptions during commit
+            print(f"Error during commit: {e}")
+            return False
 
+        
